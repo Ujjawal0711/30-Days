910709dcb259756499aafd03d9063698409fcee9
Ujjawal Chaudhary <Ujjawal.ary@gmail.com>
2025-12-25 00:08:12 +0530

    Day 1
    Implement FizzBuzz logic to print numbers from 1 to 20 with Fizz, Buzz, and FizzBuzz for multiples of 3 and 5.

--- Day01/main.c
+++ Day01/main.c
@@ -1,1 +1,18 @@
 // Day 1 Code
+
+// Goal: Print "Fizz" for multiples of 3, "Buzz" for 5, "FizzBuzz" for both.
+
+#include <stdio.h>
+int main() {
+    for (int i = 1; i <= 20; i++) {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("%d FizzBuzz\n", i);
+        else if (i % 3 == 0)
+            printf("%d Fizz\n", i);
+        else if (i % 5 == 0)
+            printf("%d Buzz\n", i);
+        else
+            printf("%d\n", i);
+    }
+    return 0;
+}
